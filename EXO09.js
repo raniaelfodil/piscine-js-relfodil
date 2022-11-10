5378148a1b1819b0241b7a6f8d1f3eff79bfd2bd
raniaelfodil <113505966+raniaelfodil@users.noreply.github.com>
2022-11-10 15:38:09 +0100

    Create EXO09.js

---
+++ EXO09.js
@@ -0,0 +1,11 @@
+function choomChoom(nb1){
+nb1 = prompt ("entrer un chiffre")
+  if (nb1%2 == 0 ) {
+    console.log ("choom")
+    
+  } else{
+    console.log ("choomchoom")
+    
+}
+}
+choomChoom();
